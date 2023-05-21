ee73672c2543f5603c0f2126cc09834aa52209f9
Hítalo Rodrigues <hitalo005@gmail.com>
2023-05-20 22:56:30 -0300

    Configuração das fontes, das imagens e do Stripes do mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,39 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground } from 'react-native'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import { styled } from 'nativewind'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-zinc-950">
-      <Text className="text-5xl font-bold text-zinc-50">
-        Open up App.js to start working on your app!
-      </Text>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-950"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2" />
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }
